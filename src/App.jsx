9e47bde76d618aaf5620cd3b0641c8a4edbe4eb3
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-10-13 16:46:58 +0300

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { AppBar } from './components/AppBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>APP-RETURN-PACK</div>
       <div>Start</div>
+      <AppBar></AppBar>
     </>
   )
 }
