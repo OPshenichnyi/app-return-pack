8d710ae0762184e7fb6342712f2cd96a4ed59469
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-11-04 20:37:13 +0200

    fix route

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- 
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/app-return-pack">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
 
 // basename = "/app-return-pack"
